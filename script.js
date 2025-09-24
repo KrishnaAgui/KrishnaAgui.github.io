519cdfda8354f33ffb6cfcb922d0ea493402b580
Impaero <impaero.kozmos@gmail.com>
2025-09-24 16:11:29 +1000

    2d folio complete

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let body = document.querySelector("#main-body");
 let header = 
 `
 <header class="main-header">
-    <div class="h-name"><a href="#" onclick="buttonClick('Home')">KRISHNA AGUINALDO</a></div>
-        <nav class="button-container">
+            <div class="h-name"><button onclick="buttonClick('Home')">KRISHNA AGUINALDO</button></div>
+            <nav class="button-container">
                 <!-- <button onclick="headerButton('3D')">3D Projects</button> -->
                 <button onclick="buttonClick('Projects')">Projects</button>
                 <button onclick="buttonClick('About')">About</button>
-         </nav>
+            </nav>
 </header>
 `;
 
@@ -33,7 +33,8 @@ let landingPage = header +
 let showcasePage = header +
 `
 <div class="showcase">
-    <div class="gallery">
+            <h1 class="showcase-section-break">ILLUSTRATIONS<h1>
+            <div class="gallery">
                 <div class="artwork" data-img="https://file.garden/aGKU5gK8lRt7Hupy/folio%20showcase/crimson%20glow%20(week2).png">
                     <img id="cglow" src="https://file.garden/aGKU5gK8lRt7Hupy/folio%20showcase/crimson%20glow%20(week2).png" alt="Crimson Glow">
                     <div class="caption">
@@ -59,10 +60,41 @@ let showcasePage = header +
                     <div class="caption">
                         <span class="art-title">"Tyranny"</span>
                         <span class="art-desc">
-                            Digital Art | 2021 <br><br>
+                            Digital Art | 2021<br><br>
                             Part of my Studio Arts work, this was inspired by oppression and tyranny. Mostly taking inspiration from mainly Frank Frazetta, this piece is paired with "Liberty."
                     </div>
                 </div>
+                <div class="artwork" data-img="https://file.garden/aGKU5gK8lRt7Hupy/folio%20showcase/dark%20forest.png">
+                    <img src="https://file.garden/aGKU5gK8lRt7Hupy/folio%20showcase/dark%20forest.png" alt="Walking through the Dark Forest">
+                    <div class="caption">
+                        <span class="art-title">Walking through the dark forest</span>
+                        <span class="art-desc">
+                            Digital Art | 2023<br><br>
+                            This was around the time I was playing Lobotomy Corporation, a game about managing a facility with entities called Abnormalities, which are born from human consciousness. This one is inspired by the Black Forest within the game, where three bird-like abnormalities come from. I aimed to capture its eerieness.
+                        </span>
+                    </div>
+                </div>
+                <div class="artwork" data-img="https://file.garden/aGKU5gK8lRt7Hupy/folio%20showcase/Aeolus%20III.png">
+                    <img src="https://file.garden/aGKU5gK8lRt7Hupy/folio%20showcase/Aeolus%20III.png" alt="Aeolus">
+                    <div class="caption">
+                        <span class="art-title">Aeolus</span>
+                        <span class="art-desc">
+                            Digital Art | 2025<br><br>
+                            Based on my own version of Flight Rising's world, this was a depiction of one of my own dragons, Aeolus. The winds and air are the focus, since he is a wind elemental dragon, focused on other's freedom.
+                        </span>
+                    </div>
+                </div>
+                <div class="artwork" data-img="https://file.garden/aGKU5gK8lRt7Hupy/folio%20showcase/Unikorpi%20-%20Geist.png">
+                    <img src="https://file.garden/aGKU5gK8lRt7Hupy/folio%20showcase/Unikorpi%20-%20Geist.png" alt="Geist (commission work)">
+                    <div class="caption">
+                        <span class="art-title">Geist (commission work)</span>
+                        <span class="art-desc">
+                            Digital Art | 2025<br><br>
+                            A commissioned artwork of an online user's (UniKorpi20) character. The keyword they gave me was a "dreamscape forest", so I focused more on the mystical and "ghostly" entities surrounding the character, as well as them interacting with one.
+                        </span>
+                    </div>
+                </div>
+
             </div>
             <!-- Lightbox modal -->
             <div id="lightbox" class="lightbox">
@@ -70,7 +102,6 @@ let showcasePage = header +
                 <img class="lightbox-img" id="lightbox-img" src="" alt="Artwork enlarged">
                 <div class="lightbox-caption" id="lightbox-caption">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nisi eu consectetur consectetur, nisl nisi consectetur nisi.</div>
             </div>
-</div>
 `
 
 // 2d project showcase
